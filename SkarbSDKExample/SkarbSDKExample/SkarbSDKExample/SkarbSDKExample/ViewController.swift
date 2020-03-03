e828c04310112e3ee7c5fbfaa17837c83f5cb7b8
Art <support@bitlica.com>
2020-03-03 17:21:37 +0300

    Removed SDK source code from example project

--- SkarbSDKExample/SkarbSDKExample/SkarbSDKExample/SkarbSDKExample/ViewController.swift
+++ SkarbSDKExample/SkarbSDKExample/SkarbSDKExample/SkarbSDKExample/ViewController.swift
@@ -15,17 +15,6 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
     
     view.backgroundColor = .red
-    
-    SKServiceRegistry.initialize()
-    
-//    SKServiceRegistry.serverAPI.sendInstall { error in
-//      print("error = \(error)")
-//    }
-//    SKServiceRegistry.serverAPI.sendSource(source: .facebook, features: ["source": "facebookTest"], completion: { error in
-//      print("error = \(error)")
-//    })
-//    SKServiceRegistry.serverAPI.sendTest(name: "testFacebook", group: "A")
-//    SKServiceRegistry.serverAPI.sendPurchase(paywall: "facebook", price: 120.99, currency: "BYN")
   }
 
 

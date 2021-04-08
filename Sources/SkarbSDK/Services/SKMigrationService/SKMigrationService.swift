04f970fe4d2ad0fb8ebff5b3947e067656ad4a1b
Artem Hitrik <artfire90@gmail.com>
2021-04-08 17:57:27 +0300

    Removed migration from v2 to v3

--- Sources/SkarbSDK/Services/SKMigrationService/SKMigrationService.swift
+++ Sources/SkarbSDK/Services/SKMigrationService/SKMigrationService.swift
@@ -17,7 +17,6 @@ struct SKMigrationService {
     
     let oldSchemaVersion = SKServiceRegistry.userDefaultsService.int(forKey: .oldSchemaVersion)
     if oldSchemaVersion < 1 {
-      migrateV2ToV3()
       migrateDeviceId(deviceId: deviceId)
       migrateFetchingProducts()
       migrateInstallCommand()
@@ -26,85 +25,6 @@ struct SKMigrationService {
     SKServiceRegistry.userDefaultsService.setValue(SKMigrationService.schemaVersion,
                                                    forKey: .oldSchemaVersion)
   }
-  
-  private func migrateV2ToV3() {
-    // isntall
-    if let installedDateISO8601 = UserDefaults.standard.string(forKey: "sk_installed_date_ISO8601"),
-      let clientId = UserDefaults.standard.string(forKey: "sk_client_id"),
-      let deviceId = UserDefaults.standard.string(forKey: "sk_device_id") {
-      
-      let appStoreReceiptURL = Bundle.main.appStoreReceiptURL
-      var dataCount: Int = 0
-      if let appStoreReceiptURL = appStoreReceiptURL,
-        let recieptData = try? Data(contentsOf: appStoreReceiptURL) {
-        dataCount = recieptData.count
-      }
-      let initData = SKInitData(clientId: clientId,
-                                deviceId: deviceId,
-                                installDate: installedDateISO8601,
-                                receiptUrl: appStoreReceiptURL?.absoluteString ?? "",
-                                receiptLen: dataCount)
-      SKServiceRegistry.userDefaultsService.setValue(initData.getData(), forKey: .initData)
-    }
-    
-    if UserDefaults.standard.bool(forKey: "sk_request_type_install") {
-      let installCommand = SKCommand(commandType: .install,
-                                     status: .done,
-                                     data: SKCommand.prepareAppgateData())
-      SKServiceRegistry.commandStore.saveCommand(installCommand)
-    }
-    
-    // test
-    if let testJSON = UserDefaults.standard.object(forKey: "sk_test_key") as? [String: Any],
-      let name = testJSON["name"] as? String,
-      let group = testJSON["group"] as? String {
-      let testData = SKTestData(name: name, group: group)
-      SKServiceRegistry.userDefaultsService.setValue(testData.getData(), forKey: .testData)
-      
-      if UserDefaults.standard.bool(forKey: "sk_request_type_test") {
-        let testCommand = SKCommand(commandType: .test,
-                                    status: .done,
-                                    data: SKCommand.prepareAppgateData())
-        SKServiceRegistry.commandStore.saveCommand(testCommand)
-      }
-    }
-    
-    //source
-    if let brokerJSONData = UserDefaults.standard.object(forKey: "sk_broker_key") as? Data,
-      let brokerJSON = try? JSONSerialization.jsonObject(with: brokerJSONData, options: []) as? [String: Any],
-      let brocker = brokerJSON["broker"] as? String,
-      let features = brokerJSON["features"] as? [AnyHashable: Any] {
-      
-      let brokerData = SKBrokerData(broker: brocker, features: features)
-      SKServiceRegistry.userDefaultsService.setValue(brokerData.getData(), forKey: .brokerData)
-      
-      if UserDefaults.standard.bool(forKey: "sk_request_type_broker") {
-        let sourceCommand = SKCommand(commandType: .source,
-                                      status: .done,
-                                      data: SKCommand.prepareAppgateData())
-        SKServiceRegistry.commandStore.saveCommand(sourceCommand)
-      }
-    }
-    
-    //purchase
-    if let productId = UserDefaults.standard.string(forKey: "sk_product_id"),
-      let currency = UserDefaults.standard.string(forKey: "sk_currency_key") {
-      let price = UserDefaults.standard.float(forKey: "sk_price_key")
-      
-      let purchaseData = SKPurchaseData(productId: productId, price: price, currency: currency)
-      SKServiceRegistry.userDefaultsService.setValue(purchaseData.getData(), forKey: .purchaseData)
-      
-      if UserDefaults.standard.bool(forKey: "sk_request_type_purchase") {
-        let purchaseCommand = SKCommand(commandType: .purchase,
-                                        status: .done,
-                                        data: SKCommand.prepareAppgateData())
-        SKServiceRegistry.commandStore.saveCommand(purchaseCommand)
-      }
-    }
-    
-    SKServiceRegistry.commandStore.saveState()
-  }
-  
   
   /// Means that user has v3 version and need to store deviceId for V4
   private func migrateDeviceId(deviceId: String) {

66334b2b525d01132e834fd8905f0a197737cb1d
Artem Hitrik <artfire90@gmail.com>
2021-04-12 13:28:44 +0300

    SPM supports 0.4.11

--- Package.swift
+++ Package.swift
@@ -4,29 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "SkarbSDK",
-    platforms: [
-        .iOS(.v11),
-    ],
-    products: [
-        .library(
-            name: "SkarbSDK",
-            targets: ["SkarbSDK"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "SkarbSDK",
-            dependencies: [],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("AdSupport"),
-                .linkedFramework("UIKit"),
-                .linkedFramework("StoreKit")
-            ]),
-        
-    ]
+  name: "SkarbSDK",
+  platforms: [
+    .iOS(.v11),
+  ],
+  products: [
+    .library(
+      name: "SkarbSDK",
+      targets: ["SkarbSDK"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/grpc/grpc-swift", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.0.0"))
+  ],
+  targets: [
+    .target(
+      name: "SkarbSDK",
+      dependencies: [],
+      linkerSettings: [
+        .linkedFramework("Foundation"),
+        .linkedFramework("AdSupport"),
+        .linkedFramework("UIKit"),
+        .linkedFramework("StoreKit")
+      ]),
+    
+  ]
 )

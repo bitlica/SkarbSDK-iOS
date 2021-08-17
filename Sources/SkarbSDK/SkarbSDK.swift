433aee65d5a858003e143689c4c32c1a3e439e5a
Artem Hitrik <artfire90@gmail.com>
2021-08-17 18:03:39 +0300

    Bumped to 0.4.15

--- Sources/SkarbSDK/SkarbSDK.swift
+++ Sources/SkarbSDK/SkarbSDK.swift
@@ -12,7 +12,7 @@ import UIKit
 public class SkarbSDK {
   
   static let agentName: String = "SkarbSDK-iOS"
-  static let version: String = "0.4.14"
+  static let version: String = "0.4.15"
   
   static var clientId: String = ""
   
